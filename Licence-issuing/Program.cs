11525377bccc59fd2982f9f043a445466446e776
yaswanth <ya8wanth@gmail.com>
2023-03-31 15:47:15 +0530

    Add project files.

---
+++ Licence-issuing/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static int LicenseIssue(string myself, int agents, string customers)
+    {
+        int iteration = 0;
+        string[] names = customers.Split(' ');
+        string[] listOfNames = new string[names.Length+1];
+        for(iteration = 0; iteration < names.Length; iteration++)
+        {
+            listOfNames[iteration] = names[iteration];
+        }
+        listOfNames[iteration] = myself;
+        Array.Sort(listOfNames);
+        int myIndex = Array.IndexOf(listOfNames, myself);
+
+        int minutes = (myIndex / agents ) * 20;
+        minutes += 20;
+        return minutes;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LicenseIssue("Eric", 2, "Adam Caroline Rebecca Frank")); 
+        Console.WriteLine(LicenseIssue("Zebediah", 1, "Bob Jim Becky Pat")); 
+        Console.WriteLine(LicenseIssue("Aaron", 3, "Jane Max Olivia Sam")); 
+    }
+}
